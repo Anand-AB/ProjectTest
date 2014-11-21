8b3df8c1ee78f70b8d6f2a96de82d85c18bc5c67
Anand <anandabktda@gmail.com>
2014-11-21 10:01:07 +0530

    Android Layouts

---
+++ src/com/anand/projecttest/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.anand.projecttest;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity2 extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vimeo);
+	}
+
+
+}
+
